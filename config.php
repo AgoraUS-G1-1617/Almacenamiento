860ade7aaf7791c9561d7f0d66e110acc7c99735
Manugdf <Manugdf94@gmail.com>
2017-01-10 13:48:59 +0100

    DB

--- config.php
+++ config.php
@@ -9,4 +9,9 @@
  define("DB_HOST", "mysql:dbname=egcdb;host=db");
  define("DB_USER", "storage_user");
  define("DB_PASS", "user");
+ 
+ $username = "test";
+ $servername ="exdb";
+ $password = "test";
+ $dbname = "egcdb";
 ?>

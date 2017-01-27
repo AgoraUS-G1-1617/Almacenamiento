3b87543de283c7d6f429e7fec42cc60aeaf95df4
manugdf <manugdf94@gmail.com>
2017-01-27 18:02:57 +0100

    Automatización de pruebas

--- tests/APITest.php
+++ tests/APITest.php
@@ -5,36 +5,37 @@ class APITest extends PHPUnit_Framework_TestCase{
 	// Get votes
 	public function testGetVotes(){
 		$id_vote = 1;
-		$url = 'http://localhost/egc/src/get_votes.php?votation_id=' . $id_vote;
+		$url = 'http://localhost/Almacenamiento/get_votes.php?votation_id=' . $id_vote;
 		$string = file_get_contents($url);
 		$data = json_decode($string,true);
 		$votes = $data["votes"];
-		$this->assertEquals(count($votes),2);
+		$msg = $data["msg"];
+		$this->assertNotEquals(count($votes),0);
+		$this->assertEquals($msg,'1');
 	}
 	
 	// Get votations
 	public function testGetVotations(){
-		$url = 'http://localhost/egc/src/get_votations.php';
+		$url = 'http://localhost/Almacenamiento/get_votations.php';
 		$string = file_get_contents($url);
 		$data = json_decode($string,true);
 		$votations = $data["votations"];
-		$this->assertEquals(count($votations),1);
+		$this->assertEquals(count($votations),4);
 	}
 	
 	// Vote
 	public function testVote(){	
-		$jsondata['vote'] = 'VotoPruebaTest';
-		$jsondata['votation_id'] = "3";
-		$url = 'http://localhost/egc/src/vote.php';
 		$ch = curl_init();
+		$url = 'http://localhost/Almacenamiento/vote.php';
+		$json = '{"age":1000,"id":"100","autonomous_comunity":"Prueba","genre":"prueba","id_poll":3,
+  					"answers":[{"question":"Pregunta 1","answers_question":"SI"},
+             		{"question":"Pregunta 2","answers_question":"NO"}]}';
 		curl_setopt($ch, CURLOPT_URL, $url);
 		curl_setopt($ch, CURLOPT_POST, 1);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($jsondata));
+		curl_setopt($ch, CURLOPT_POSTFIELDS,$json);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 		
 		$response = curl_exec($ch);
-		curl_close($ch);
-		
 		$this->assertEquals($response,'{"msg":"1"}');
 		
 	}	

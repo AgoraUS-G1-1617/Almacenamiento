53a59e5a4d1f17e60d683133ac72809d0a8fad7b
Manugdf <Manugdf94@gmail.com>
2017-02-01 18:54:34 +0100

    debug login

--- services/checkLogin.php
+++ services/checkLogin.php
@@ -57,11 +57,13 @@ function file_get_contents_curl($url) {
     $ch = curl_init();
 
     curl_setopt($ch, CURLOPT_AUTOREFERER, TRUE);
+	curl_setopt($ch, CURLOPT_POST, 1 );
+	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
     curl_setopt($ch, CURLOPT_HEADER, 0);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE); 
-	curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,FALSE);      
+	curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,0);      
 
     $data = curl_exec($ch);
     curl_close($ch);
